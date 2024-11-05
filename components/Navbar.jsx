54d30119f60ea0a782c69f3612207aa181e76765
AviraLS22 <aviralsharma.sharma2205@gmail.com>
2024-11-05 23:37:46 +0530

    edited navbut

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -28,8 +28,8 @@ const Navbar = ({ showHomeOnly = false }) => {
       whileInView="show"
       className={`${styles.xPaddings} py-8 relative flex items-center`}
     >
-      <div className="absolute w-[50%] " />
-      <div className={`${styles.innerWidth} mx-auto flex justify-between items-center `}>
+      <div className="absolute w-[50%]" />
+      <div className={`${styles.innerWidth} mx-auto flex justify-between items-center`}>
         
         {/* Logo Image */}
         <div className="logo-container">
@@ -44,18 +44,44 @@ const Navbar = ({ showHomeOnly = false }) => {
         </div>
 
         {/* Navbar buttons for large screens */}
-        <div className="hidden lg:flex space-x-4 gap-10" >
+        <div className="hidden lg:flex space-x-4 gap-10">
           {showHomeOnly ? (
             <Link href="/" passHref>
-              <button className="ordbut text-white hover:text-gray-300">Home</button>
+              <motion.button
+                className="ordbut text-white hover:text-gray-300"
+                whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                whileTap={{ scale: 0.9 }}
+              >
+                Home
+              </motion.button>
             </Link>
           ) : (
             <>
-              <button onClick={() => handleScrollToSection('about-section')} className="ordbut text-white hover:text-gray-300">About</button>
+              <motion.button
+                onClick={() => handleScrollToSection('about-section')}
+                className="ordbut text-white hover:text-gray-300"
+                whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                whileTap={{ scale: 0.9 }}
+              >
+                About
+              </motion.button>
               <Link href="/team" passHref>
-                <button className="teambut pt-3 text-white hover:text-gray-300">Team</button>
+                <motion.button
+                  className="teambut pt-3 text-white hover:text-gray-300"
+                  whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                  whileTap={{ scale: 0.9 }}
+                >
+                  Team
+                </motion.button>
               </Link>
-              <button onClick={() => handleScrollToSection('explore')} className="ordbut text-white hover:text-gray-300">Events</button>
+              <motion.button
+                onClick={() => handleScrollToSection('explore')}
+                className="ordbut text-white hover:text-gray-300"
+                whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                whileTap={{ scale: 0.9 }}
+              >
+                Events
+              </motion.button>
               <button onClick={() => handleScrollToSection('contact')} className="glow-button">REGISTER NOW</button>
             </>
           )}
@@ -74,15 +100,43 @@ const Navbar = ({ showHomeOnly = false }) => {
             <div className="lg:hidden fixed top-20 right-0 w-full bg-violet-900 shadow-lg p-5 z-40">
               {showHomeOnly ? (
                 <Link href="/" passHref>
-                  <button onClick={toggleMenu} className="block w-full py-2 text-white text-left hover:text-gray-300">Home</button>
+                  <motion.button
+                    onClick={toggleMenu}
+                    className="block w-full py-2 text-white text-left hover:text-gray-300"
+                    whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    Home
+                  </motion.button>
                 </Link>
               ) : (
                 <>
-                  <button onClick={() => handleScrollToSection('about-section')} className="block w-full py-2 text-white text-left hover:text-gray-300">About</button>
+                  <motion.button
+                    onClick={() => handleScrollToSection('about-section')}
+                    className="block w-full py-2 text-white text-left hover:text-gray-300"
+                    whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    About
+                  </motion.button>
                   <Link href="/team" passHref>
-                    <button onClick={toggleMenu} className="block w-full py-2 text-white text-left hover:text-gray-300">Team</button>
+                    <motion.button
+                      onClick={toggleMenu}
+                      className="block w-full py-2 text-white text-left hover:text-gray-300"
+                      whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                      whileTap={{ scale: 0.95 }}
+                    >
+                      Team
+                    </motion.button>
                   </Link>
-                  <button onClick={() => handleScrollToSection('explore')} className="block w-full py-2 text-white text-left hover:text-gray-300">Events</button>
+                  <motion.button
+                    onClick={() => handleScrollToSection('explore')}
+                    className="block w-full py-2 text-white text-left hover:text-gray-300"
+                    whileHover={{ scale: 1.8, color: "#d3d3d3" }}
+                    whileTap={{ scale: 0.95 }}
+                  >
+                    Events
+                  </motion.button>
                   <button onClick={() => handleScrollToSection('contact')} className="block w-full py-2 glow-button">REGISTER NOW</button>
                 </>
               )}
