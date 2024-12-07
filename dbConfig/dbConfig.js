38e403c5990cf57051df572a9eca26f8f748d3d4
AviraLS22 <aviralsharma.sharma2205@gmail.com>
2024-12-07 18:26:09 +0530

    updated env variable

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -9,13 +9,10 @@ const connectMongo = async () => {
   }
 
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(process.env.MONGO_URI); // No need for deprecated options
 
     isConnected = db.connections[0].readyState; // 1 means connected
-    console.log("MongoDB Connected");
+    console.log("MongoDB Connected Successfully");
     return db.connection;
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
