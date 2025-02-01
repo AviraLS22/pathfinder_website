b4d267df83c5d5f3fd09edc87ee762ac54e6aef9
AviraLS22 <aviralsharma.sharma2205@gmail.com>
2025-02-01 22:48:36 +0530

    academic fixed

--- components/Alumini.js
+++ components/Alumini.js
@@ -2,32 +2,32 @@ export const AluminiMembers = [
     // 4th years
     {
       name: 'Pruthvi',
-      academicYear: '2020-2024',
+      academicYear: '2019-2023',
       profilePic: '/Pruthvi.jpeg',
     },
     {
         name: 'Pragati Shankar',
-        academicYear: '2020-2024',
+        academicYear: '2019-2023',
         profilePic: '/pragati shankar.jpeg',
       },
       {
         name: 'Monish',
-        academicYear: '2020-2024',
+        academicYear: '2019-2023',
         profilePic: '/monish.jpeg',
       },
       {
         name: 'Abhineet',
-        academicYear: '2020-2024',
+        academicYear: '2019-2023',
         profilePic: '/abhineet.jpeg',
       },
       {
         name: 'Aman Narendra',
-        academicYear: '2020-2024',
+        academicYear: '2019-2023',
         profilePic: '/amannarendra.jpeg',
       },
       {
         name: 'Wasim',
-        academicYear: '2020-2024',
+        academicYear: '2019-2023',
         profilePic: '/wasim.jpeg',
       },
 ];
